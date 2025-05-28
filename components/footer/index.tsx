40a461d698aa1ae59e96cd1dd7150b829d713e4e
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-05-28 11:22:46 -0300

    Add copy in the site

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -5,8 +5,16 @@ export default function Footer() {
   return (
     <footer className="w-screen h-auto">
       <FooterContainer>
-      <FooterInfo />
+        <FooterInfo />
+        <div className="w-11/12 p-2 mt-2 border-t border-slate-100 flex justify-center items-center gap-2 max-sm:flex-col">
+          <span className="text-slate-100/60 text-sm">
+            © {new Date().getFullYear().toString()} EducaVales
+          </span>
+          <span className="text-slate-100/60 text-sm">
+            Todos os direitos reservados
+          </span>
+        </div>
       </FooterContainer>
     </footer>
-  )
+  );
 }
