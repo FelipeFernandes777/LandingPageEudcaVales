a8b6fad90505f89df3749097b70378c8c82c5b66
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-05-30 11:43:36 -0300

    Fix: image card courses for sm

--- components/courses/courses_card/courses_card_container/index.tsx
+++ components/courses/courses_card/courses_card_container/index.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export function CoursesCardContainer({children}: {children: ReactNode}){
   return (
     <div className="w-full h-[250px] shadow-md border-slate-200 border rounded-2xl relative bg-slate-50 flex overflow-hidden
-      md:flex flex-col md:h-[450px]
+      md:flex md:flex-col md:h-[450px]
     ">
       {children}
     </div>
