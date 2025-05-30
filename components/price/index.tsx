139ab7d18130680b47c67169c21264d46de042fb
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-05-30 11:37:25 -0300

    Create div for price component, for resposivity in tablet

--- components/price/index.tsx
+++ components/price/index.tsx
@@ -62,9 +62,11 @@ export default function Price() {
     <PriceContainer>
       <div className="w-11/12 border-t border-slate-200"></div>
       <PriceTitle>Investimento</PriceTitle>
-      {priceCards.map((item, i) => (
-        <PriceCard {...item} key={i} />
-      ))}
+      <div className="w-full h-auto flex gap-4 p-4 max-sm:flex-col max-sm:p-0 items-center justify-center">
+        {priceCards.map((item, i) => (
+          <PriceCard {...item} key={i} />
+        ))}
+      </div>
     </PriceContainer>
   );
 }
