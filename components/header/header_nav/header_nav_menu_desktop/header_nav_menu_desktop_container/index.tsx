9f904ce132a6829bb0b69d6e34fde8dc42fda78c
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-05-30 14:02:12 -0300

    Fix header width

--- components/header/header_nav/header_nav_menu_desktop/header_nav_menu_desktop_container/index.tsx
+++ components/header/header_nav/header_nav_menu_desktop/header_nav_menu_desktop_container/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function HeaderNavMenuDesktopContainer({children}: {children: ReactNode}){
   return (
-    <div className="w-6/12 h-20 flex justify-evenly items-center max-lg:hidden"> 
+    <div className="w-7/12 h-20 flex justify-evenly items-center max-lg:hidden"> 
       {children}
     </div>
   )
