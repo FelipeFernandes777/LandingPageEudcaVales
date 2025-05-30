fd4e3e56efbf70589be50406ad6664043676e10c
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-05-30 13:38:20 -0300

    Add shadow in price card

--- components/price/price_card/price_card_container/index.tsx
+++ components/price/price_card/price_card_container/index.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from "react";
 
 export function PriceCardContainer({children}: {children: ReactNode}){
   return (
-    <div className="w-11/12 border border-gray-400/50 flex items-center justify-center h-[500px] rounded-3xl overflow-hidden flex-col lg:w-5/12">{children}</div>
+    <div className="w-11/12 border border-gray-400/50 flex items-center justify-center h-[500px] rounded-3xl overflow-hidden flex-col lg:w-5/12 shadow-md">{children}</div>
   )
 }
