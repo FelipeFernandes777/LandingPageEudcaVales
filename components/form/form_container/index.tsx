f37bfc5869db173fa0cc9344ba59cd2c6bcf8e28
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-05-30 13:32:34 -0300

    modify form component for laptop

--- components/form/form_container/index.tsx
+++ components/form/form_container/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function FormContainer({children}: {children: ReactNode}){
   return (
-    <div className="w-full h-auto py-2 mt-5 flex flex-col items-center gap-2">
+    <div className="w-full h-[600px] py-2 mt-5 flex flex-col items-center gap-2">
       <div className="w-11/12 border-t border-slate-300"></div>
       {children}
     </div>
--- components/form/index.tsx
+++ components/form/index.tsx
@@ -1,12 +1,23 @@
+import Image from "next/image";
 import { FormCard } from "./form_card";
 import { FormContainer } from "./form_container";
 import { FormTitle } from "./form_title";
 
-export default function Form(){
+export default function Form() {
   return (
     <FormContainer>
-      <FormTitle title="Entre em contato"/>
-      <FormCard />
+      <FormTitle title="Entre em contato" />
+      <div className="w-full h-full flex gap-3 justify-center items-center px-5">
+        <div className="w-full h-full relative hidden lg:block">
+          <Image
+            src={"/form/Checklist.jpg"}
+            alt="Imagem de um checklist"
+            fill
+            className="object-contain"
+          />
+        </div>
+        <FormCard />
+      </div>
     </FormContainer>
-  )
+  );
 }
