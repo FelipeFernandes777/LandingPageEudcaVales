265f13158a09e0e367448cce4019d61b79a64705
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-06-04 10:49:31 -0300

    alter size in my price

--- components/price/price_title/index.tsx
+++ components/price/price_title/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export function PriceTitle({ children }: { children: ReactNode }) {
-  return <h1 className="text-2xl font-medium text-[var(--black-color)]">{children}</h1>;
+  return <h1 className="text-2xl lg:text-3xl font-medium text-[var(--black-color)]">{children}</h1>;
 }
