29e70dfcedaa6e839b25cbee1882926f0b3f909b
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-06-05 10:46:43 -0300

    Modify card course background

--- components/courses/courses_card/courses_card_container/index.tsx
+++ components/courses/courses_card/courses_card_container/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function CoursesCardContainer({children}: {children: ReactNode}){
   return (
-    <div className="w-full h-[250px] shadow-md border-slate-200 border rounded-2xl relative bg-[var(--white-color)] flex overflow-hidden
+    <div className="w-full h-[250px] shadow-md border-slate-200 border rounded-2xl relative bg-white flex overflow-hidden
       md:flex md:flex-col md:h-[450px]
       lg:flex-row lg:h-[250px]
     ">
