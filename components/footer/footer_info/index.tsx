8e96aed494aa0eef7690d3c4b034fce68e33d557
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-06-06 10:15:12 -0300

    add links for instagram and facebook

--- components/footer/footer_info/index.tsx
+++ components/footer/footer_info/index.tsx
@@ -5,9 +5,8 @@ import { FooterInfoTitle } from "./footer_info_title/intex";
 
 export function FooterInfo() {
   const links = {
-    instagram: "",
-    facebook: "",
-    linkedin: "",
+    instagram: "https://www.instagram.com/educavale.oficial?igsh=MW1vcWY4cWZ4ZGxrMg==",
+    facebook: "https://www.facebook.com/Educavale/",
   };
 
   return (
@@ -52,24 +51,6 @@ export function FooterInfo() {
               className="lucide lucide-facebook-icon lucide-facebook text-slate-100"
             >
               <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-            </svg>
-          </a>
-          <a href={links.linkedin}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-linkedin-icon lucide-linkedin text-slate-100"
-            >
-              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-              <rect width="4" height="12" x="2" y="9" />
-              <circle cx="4" cy="4" r="2" />
             </svg>
           </a>
         </div>
