d2c1d6b4eb4593646ac3d410d9981d16f3eae99c
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-06-09 11:40:10 -0300

    Modify Menu Float

--- components/benneficies/benneficies_float/benneficies_float_card/benneficies_float_card_text/index.tsx
+++ components/benneficies/benneficies_float/benneficies_float_card/benneficies_float_card_text/index.tsx
@@ -1,6 +1,6 @@
 
 export function BenneficiesFloatCardText({text}: {text:string}){
   return (
-    <p className="font-medium text-[var(--black-color)] tracking-tight">{text}</p>
+    <p className="font-medium text-[var(--green-color)] tracking-tight">{text}</p>
   )
 }
--- components/benneficies/benneficies_float/benneficies_float_container/index.tsx
+++ components/benneficies/benneficies_float/benneficies_float_container/index.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 export function BenneficiesFloatContainer({children}: {children: ReactNode}) {
   return (
-    <div className="w-11/12 h-[120px] translate-y-[-25px] hidden md:flex px-2 shadow-lg shadow-[var(--light-green-color)]-100 border border-[var(--green-color)] rounded-xl gap-2 bg-white overflow-hidden absolute
-      lg:w-9/12
+    <div className="w-11/12 h-[120px] translate-y-[-25px] hidden md:flex px-2 shadow-lg shadow-[var(--light-green-color)]-100 border border-[var(--green-color)] rounded-xl gap-2 bg-[var(--light-green-color)] overflow-hidden absolute
+      lg:w-7/12
     ">
       {children}
     </div>
