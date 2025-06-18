f25c236679524bbb4f9b2493854c1bac2d852a12
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-06-18 15:47:31 -0300

    Finish Project

--- components/banner/banner_image/index.tsx
+++ components/banner/banner_image/index.tsx
@@ -5,21 +5,21 @@ export function BannerImage() {
     <div className="w-full h-full relative">
       <picture>
         <source
-          srcSet="/banner/banner-xlarge.jpeg"
+          srcSet="/banner/banner-xlarge.png"
           media="(min-width: 1920px)"
         />
         <source
-          srcSet="/banner/banner-large.jpeg"
+          srcSet="/banner/banner-large.png"
           media="(min-width: 1440px)"
         />
         <source
-          srcSet="/banner/banner-medium.jpeg"
+          srcSet="/banner/banner-medium.png"
           media="(min-width: 640px)"
         />
-        <source srcSet="/banner/banner-small.jpeg" media="(max-width: 639px)" />
+        <source srcSet="/banner/banner-small.png" media="(max-width: 639px)" />
         <Image
           alt="Banner ilustrativo"
-          src="/banner/banner-medium.jpeg"
+          src="/banner/banner-medium.png"
           fill
           priority
           className="object-cover"
