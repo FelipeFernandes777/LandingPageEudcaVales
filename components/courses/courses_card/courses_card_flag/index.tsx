ad887f003936270b7189af3f9671da011596127d
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-06-24 08:09:08 -0300

    fix flag in mobile

--- components/courses/courses_card/courses_card_flag/index.tsx
+++ components/courses/courses_card/courses_card_flag/index.tsx
@@ -1,6 +1,6 @@
 export function CoursesCardFlag() {
   return (
-    <div className="lg:w-2/12 max-sm:w-5/12 bg-[var(--light-green-color)] p-2 flex items-center justify-center rounded-3xl self-center absolute lg:bottom-2 lg:left-2 max-[769px]:top-1 md:w-5/12">
+    <div className="lg:w-2/12 max-sm:w-5/12 bg-[var(--light-green-color)] p-2 flex items-center justify-center rounded-3xl self-center absolute lg:bottom-2 lg:left-2 max-[769px]:top-1 md:w-5/12 max-[426px]:left-2 max-[426px]:bottom-[-200px]">
       <p className="text-[var(--black-color)] font-medium w-full text-center">
         100% EAD
       </p>
