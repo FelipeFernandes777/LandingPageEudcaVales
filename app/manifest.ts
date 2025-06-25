99b0702159d402334e18b6df756b4847573b0c0c
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-06-25 08:55:12 -0300

    modify manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,14 +2,20 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest ():MetadataRoute.Manifest {
     return {
-        name:"LandingPage Educavale",
-        short_name: 'LP Educavale',
-        start_url: "/index.html",
-        description: "Landing Page educacional, para venda de cursos de pós graduação.",
+        name: "LandingPage Educavale",
+        short_name: "LP Educavale",
+        start_url: "/",
+        description: "Landing page educacional para venda de cursos de pós-graduação.",
         background_color: "#FFFFFF",
         theme_color: "#1a73e8",
         display: "standalone",
         icons: [
+            {
+                src: "/favicon16.png",
+                sizes: "16x16",
+                type: "image/png",
+                purpose: "maskable"
+            },
             {
                 src: "/favicon32.png",
                 sizes: "32x32",
