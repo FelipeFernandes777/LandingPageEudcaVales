34b112877b500635942ed442e26e2886b8624373
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-06-25 09:45:11 -0300

    Add favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,9 +90,9 @@ export default function RootLayout({
             }),
           }}
         />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon32.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="https://faculdadeeducavale.com.br/manifest.webmanifest" />
         <meta name="google-site-verification" content="rDAI_CmrSUIzf-NmBpAuUWh0EyQNG28DE9iUXRSdds4" />
       </head>
