14204058afe51645a9f9be9cf543b57f7c7884b2
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-07-01 10:04:21 -0300

    finish redirect-page

--- app/(thank)/redirect-whatsapp/page.tsx
+++ app/(thank)/redirect-whatsapp/page.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
 
 export default function ThanksPage(){
     return (
-        <div>
-            <p>Teste</p>
+        <div className="w-full h-screen gap-10 flex justify-center items-center flex-col">
+            <div className="w-6/12 flex justify-center items-center">
+                <Image
+                    src="/whatsapp_icon.webp"
+                    alt={"Imagem do whatsapp"}
+                    width={100}
+                    height={100}
+                />
+            </div>
+            <p className="font-medium text-5xl">
+                Aguarde...
+            </p>
+            <p className="font-medium text-3xl">
+                Em breve um dos nossos consultores entrará em contato no contato informado!
+            </p>
         </div>
     )
 }
