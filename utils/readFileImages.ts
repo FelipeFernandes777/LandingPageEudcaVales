a784b65f0b2bc8f4794e657d63e37b9529329815
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-07-04 15:02:27 -0300

    fix sitemap xml

--- utils/readFileImages.ts
+++ utils/readFileImages.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 export function readFileImages(pathFile:string) {
     const imagePath = path.join(process.cwd(), pathFile);
     return fs.readdirSync(imagePath)
-        .filter(file => /\.(jpe?g|png|svg)$/i.test(file))
+        .filter(file => /\.(jpe?g|png|svg|webp)$/i.test(file))
         // .map(file => `${process.env.BASE_URL || "https://faculdadeeducavale.com.br"}/${pathFile.split("public/")}/${file}`);
         .map(file => {
             const relativePath = path.relative("public", pathFile);
